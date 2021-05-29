18bec42cdc22f0edf693c2f96b026695ea446b4c
FrodoCompacto <jvfaggion@hotmail.com>
2021-05-29 00:30:36 -0300

    Added CORS configuration

--- src/main/kotlin/foundryrest/io/Application.kt
+++ src/main/kotlin/foundryrest/io/Application.kt
@@ -3,9 +3,16 @@ package foundryrest.io
 import io.ktor.server.engine.*
 import io.ktor.server.netty.*
 import foundryrest.io.plugins.*
+import io.ktor.application.*
+import io.ktor.features.*
+import io.ktor.http.*
 
 fun main() {
     embeddedServer(Netty, port = 8080, host = "0.0.0.0") {
         configureRouting()
+        install(CORS){
+            method(HttpMethod.Get)
+            anyHost()
+        }
     }.start(wait = true)
 }
